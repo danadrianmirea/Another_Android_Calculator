c328ce6525c48de73ac7634ce091dd1b135f01a8
Dan Adrian Mirea <damirea84@gmail.com>
2024-10-23 20:12:55 +0300

    first commit

---
+++ init.bat
@@ -0,0 +1,7 @@
+echo "# Another_Android_Calculator" >> README.md
+git init
+git add .
+git commit -m "first commit"
+git branch -M main
+git remote add origin git@github.com:danadrianmirea/Another_Android_Calculator.git
+git push -u origin main
